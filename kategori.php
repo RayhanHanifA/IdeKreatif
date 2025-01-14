d5d0d8a5d4edf56058497428ff819a2280de665a
RayhanHanifA <rayhanasmoro329@gmail.com>
2025-01-14 14:44:43 +0700

    kategory

---
+++ kategori.php
@@ -0,0 +1,84 @@
+<?php
+// Memasukkan header halaman
+include 'includes/header.php';
+// Menyertakan file untuk menampilkan notifikasi (jika ada)
+include 'includes/toast_notification.php';
+?>
+
+<div class="container-xxl flex-grow-1 container-p-y">
+  <div class="card">
+    <div class="card-header d-flex justify-content-between align-items-center">
+      <h4>Data Kategori</h4>
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addCategory">
+        Tambah Kategori
+      </button>
+    </div>
+
+    <div class="card-body">
+      <div class="table-responsive text-nowrap">
+        <table id="datatable" class="table table-hover">
+          <thead>
+            <tr class="text-center">
+              <th width="50px">#/No</th>
+              <th>Nama</th>
+              <th width="150px">Pilihan</th>
+            </tr>
+          </thead>
+          <tbody class="table-border-bottom-0">
+            <?php
+            // Mengambil data kategori dari database
+            $index = 1;
+            $query = "SELECT * FROM categories"
+            $exec = mysqli_query($conn, $query);
+            while ($category = mysqli_fetch_asoc($exec));
+            ?>
+                <tr>
+                  <?= $index ?></td>
+                  <td><?= $category['category_name'] ?></td>
+
+                    <div class="dropdown">
+                      <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                        <i class="bx bx-dots-vertical-rounded"></i>
+                      </button>
+                      <div class="dropdown-menu">
+                        <a href="#" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#editCategory<?= $category['category_id'] ?>">
+                          <i class="bx bx-edit-alt me-2"></i> Edit
+                        </a>
+                        <a href="#" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#deleteCategory<?= $category['category_id'] ?>">
+                          <i class="bx bx-trash me-2"></i> Delete</a>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+
+              <?php endwhile; ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include'<div class="includes/footer.php'; ?>
+
+
+
+<div class="modal fade" id="addCategory" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+  <div class="modal-content">
+    <div class="modal-header">
+    <h5 class="modal-title">Tambah Data</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <form action="proses_kategori.php" method="POST">
+    <div class="modal-body">
+      <label for="namaKategori" class="form-label">Nama Kategori</label>
+      <input type="text" class="form-control" name="category_name" required>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Batal</button>
+      <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+    </div>
+    </form>
+  </div>
+  </div>
+</div>
